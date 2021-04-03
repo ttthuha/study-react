d22c87bc8c2c393778056b02e4ed19504df393df
ttthuha <thuhatrinh.3011@gmail.com>
2021-04-03 14:00:00 +0700

    study function

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import NewCom from './NewComponent'
 
 const x = <div>abc</div>
 const X = ({ name }) => <div>{name}</div>
-
-
+const func = ()=> {console.log('call from parents')}
 
 const App = () => {
 
@@ -14,12 +13,10 @@ const App = () => {
   </div>
 
   return (
-    <div className="App">
-      <NewCom name="Ha" age={20} gender="Nu" />
-      {NewCom({ name: "Ha", age: 20, gender: "nu" })}
-      {x}
-      <X name="ha" />
-      <X2 cl="1" school="abc" />
+    <div className='App'>
+      <NewCom name="Ha" age={20} gender="Nu" func = {func}/>
+      {NewCom({ name: "Ha", age: 20, gender: "nu", func: func})}
+      
     </div>
   );
 }
--- src/NewComponent.js
+++ src/NewComponent.js
@@ -1,10 +1,14 @@
-function newCom(props){
-const {name, age, gender}= props;
-return (
-    <div>
-        name: {name}, age: {age}, gender: {gender}
-    </div>
-)
+function newCom(props) {
+    const { name, age, gender, func } = props;
+    //const func = () => { console.log('call me') } 
+    
+    return (
+        <div>
+            name: {name}, age: {age}, gender: {gender}
+            <button onClick={func}> Click</button>
+            <button onClick = {() => { console.log('call me 2')}}> Click 2</button>
+        </div>
+    )
 }
 
 export default newCom;
